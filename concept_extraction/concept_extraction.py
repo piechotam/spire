43a9cd4d102b508d55d36364403c441c2904b022
krukj <126516292+krukj@users.noreply.github.com>
2025-05-04 17:03:22 +0200

    example usage - president election and animals

--- concept_extraction/concept_extraction.py
+++ concept_extraction/concept_extraction.py
@@ -10,6 +10,7 @@ from sparse_autoencoder.autoencoder.model import SparseAutoencoder
 _SAE_N_INPUT_FEATURES = 512
 _SAE_CONCEPTS_SCALING = 8
 
+
 def load_concepts(file_path: str, column_index: int = 1) -> tuple:
     """
     Loads a specific column from a CSV file into a Python tuple efficiently.
@@ -18,7 +19,7 @@ def load_concepts(file_path: str, column_index: int = 1) -> tuple:
     ----------
     file_path: str
         The path to the CSV file.
-    
+
     column_index: int
         The zero-based index of the column to load.
 
@@ -30,7 +31,7 @@ def load_concepts(file_path: str, column_index: int = 1) -> tuple:
     """
 
     try:
-        with open(file_path, 'r', newline='') as csvfile:
+        with open(file_path, "r", newline="") as csvfile:
             reader = csv.reader(csvfile)
             column_data = tuple(row[column_index] for row in reader if row)
         return column_data
@@ -44,8 +45,11 @@ def load_concepts(file_path: str, column_index: int = 1) -> tuple:
         print(f"An error occurred: {e}")
         return ()
 
-def load_sae(checkpoint_path: str,
-             device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu") -> torch.nn.Module:
+
+def load_sae(
+    checkpoint_path: str,
+    device: Union[str, torch.device] = "cuda" if torch.cuda.is_available() else "cpu",
+) -> torch.nn.Module:
     """
     Load a Sparse Autoencoder (SAE) model from a checkpoint file.
 
@@ -53,7 +57,7 @@ def load_sae(checkpoint_path: str,
     ----------
     checkpoint_path: str
         The path to the checkpoint file.
-    
+
     device: Union[str, torch.device], optional
         The device to load the model on. Defaults to "cuda" if available, else "cpu".
 
@@ -64,15 +68,17 @@ def load_sae(checkpoint_path: str,
         Returns None if the checkpoint file is not found or an error occurs.
     """
     try:
-        sae_state_dict = torch.load(checkpoint_path,
-                                    map_location=torch.device(device),
-                                    weights_only=True)
-        
+        sae_state_dict = torch.load(
+            checkpoint_path, map_location=torch.device(device), weights_only=True
+        )
+
         for param_name, param_tensor in sae_state_dict.items():
             sae_state_dict[param_name] = param_tensor.squeeze()
-        
-        sparse_autoencoder = SparseAutoencoder(n_input_features=_SAE_N_INPUT_FEATURES,
-                                               n_learned_features=_SAE_N_INPUT_FEATURES * _SAE_CONCEPTS_SCALING)
+
+        sparse_autoencoder = SparseAutoencoder(
+            n_input_features=_SAE_N_INPUT_FEATURES,
+            n_learned_features=_SAE_N_INPUT_FEATURES * _SAE_CONCEPTS_SCALING,
+        )
         sparse_autoencoder.load_state_dict(sae_state_dict)
         return sparse_autoencoder
     except FileNotFoundError:
@@ -85,7 +91,10 @@ def load_sae(checkpoint_path: str,
         print(f"An error occurred: {e}")
         return None
 
-def prepare_image(image_path: str, preprocess: torchvision.transforms.transforms.Compose):
+
+def prepare_image(
+    image_path: str, preprocess: torchvision.transforms.transforms.Compose
+):
     """
     Load and transform image.
 
@@ -93,7 +102,7 @@ def prepare_image(image_path: str, preprocess: torchvision.transforms.transforms
     ----------
     image_path: str
         The path to the image.
-    
+
     preprocess: torchvision.transforms.transforms.Compose
         A torchvision transform that converts a PIL image into a tensor.
 
@@ -106,11 +115,23 @@ def prepare_image(image_path: str, preprocess: torchvision.transforms.transforms
 
     return image, transformed_image
 
-def extract_concepts(n_concepts: int,
-                     concept_names: tuple,
-                     image_transformed: torch.Tensor,
-                     clip_model: torch.nn.Module,
-                     sae: torch.nn.Module):
+
+def prepare_image_from_datasets(
+    image: Image, preprocess: torchvision.transforms.transforms.Compose
+):
+    image = image.convert("RGB")
+
+    transformed_image = preprocess(image).unsqueeze(0)
+    return image, transformed_image
+
+
+def extract_concepts(
+    n_concepts: int,
+    concept_names: tuple,
+    image_transformed: torch.Tensor,
+    clip_model: torch.nn.Module,
+    sae: torch.nn.Module,
+):
     """
     Extract top concepts from an image.
 
@@ -118,33 +139,37 @@ def extract_concepts(n_concepts: int,
     ----------
     n_concepts: int
         Number of concepts to extract.
-    
+
     concept_names: tuple
         Tuple of named concepts.
-    
+
     image_transformed: torch.Tensor
         Tensor with transformed image.
-    
+
     clip_model: torch.nn.Module
         Clip model for creating image embedding.
 
     sae: torch.nn.Module
         Sparse autoencoder for extracting concepts from clip embedding.
-    
+
     Returns
     -------
     top_n_concept_activations: np.ndarray
         Activations values for top n_concepts concepts.
-    
+
     top_n_concept_names: list
         Names of top n_concepts concepts.
     """
     image_clip_embedding = clip_model.encode_image(image_transformed)
     concept_activations, _ = sae(image_clip_embedding)
     concept_activations_numpy = concept_activations.squeeze().detach().numpy()
-    
-    indices_of_largest = np.argpartition(concept_activations_numpy, -n_concepts)[-n_concepts:]
-    largest_indices_sorted = indices_of_largest[np.argsort(concept_activations_numpy[indices_of_largest])]
+
+    indices_of_largest = np.argpartition(concept_activations_numpy, -n_concepts)[
+        -n_concepts:
+    ]
+    largest_indices_sorted = indices_of_largest[
+        np.argsort(concept_activations_numpy[indices_of_largest])
+    ]
 
     top_n_concept_activations = concept_activations_numpy[largest_indices_sorted]
     top_n_concept_names = [concept_names[i] for i in largest_indices_sorted]
