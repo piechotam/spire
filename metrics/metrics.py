66d20c184ffcd3c3c12e81bc1b562edef1cdc816
Michał Piechota <michalpiechotaa13@gmail.com>
2025-06-10 14:45:07 +0200

    top_k filtering, gt similarities

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -6,6 +6,8 @@ from typing import Dict, List, Set, Tuple, Literal
 from sklearn.metrics.pairwise import cosine_similarity, manhattan_distances
 from math import ceil
 
+REFERENCE_SET_SIZE = 50000
+
 def create_ground_truth_dict(path_to_gt_file: str) -> Dict[int, int]:
     '''
     Creates ground truth dict where ground_truth[idx] is the index of the relevant reference item for query with index idx.
@@ -138,6 +140,13 @@ def recall_top_k(
 
     return true_positive / n_positives if n_positives > 0 else 0.0
 
+def _filter_top_k_activations(
+        sae_embeddings: np.ndarray,
+        top_k):
+    valid_top_k = min(top_k, sae_embeddings.shape[1])
+    kth_values = np.partition(sae_embeddings, -valid_top_k, axis=1)[:, -valid_top_k]
+    sae_embeddings[sae_embeddings < kth_values[:, np.newaxis]] = 0
+
 def _process_single_reference(
         nearest_neighbors: Dict[int, List[Tuple[float, int, bool]]],
         reference_embeddings_path: str,
@@ -149,7 +158,8 @@ def _process_single_reference(
         query_indices_fileref: h5py.Dataset,
         n_queries: int,
         k_nearest: int,
-        batch_size: int
+        batch_size: int,
+        top_k: int | None = None
 ):
     '''
     Processes single reference set by finding k_nearest neighbors embeddings from that set.
@@ -162,6 +172,12 @@ def _process_single_reference(
     with h5py.File(f"{reference_embeddings_path}/{model_embeddings_filename}") as reference_embeddings_file, \
          h5py.File(f"{reference_embeddings_path}/{indices_filename}") as reference_indices_file:
         reference_embeddings = reference_embeddings_file[f"{model}_embeddings"][:]
+        
+        if model == "sae" and top_k is not None:
+            print(f"Applying top_k={top_k} filtering to SAE reference embeddings...")
+            _filter_top_k_activations(reference_embeddings, top_k)
+            print("Finished applying top_k filtering.")
+
         reference_indices = reference_indices_file["image_indices"][:]
         k_nearest = min(k_nearest, reference_embeddings.shape[0])
         
@@ -180,6 +196,9 @@ def _process_single_reference(
             if start_idx >= end_idx: break
             query_embeddings_batch = query_embeddings_fileref[start_idx:end_idx]
             
+            if model == "sae" and top_k is not None:
+                _filter_top_k_activations(query_embeddings_batch, top_k)
+
             if model == "sae" and faiss_index is not None:
                 query_embeddings_batch_faiss = np.ascontiguousarray(query_embeddings_batch.astype('float32'))
                 distances, nearest_neighbors_array_indices_k_nearest = faiss_index.search(query_embeddings_batch_faiss, k_nearest)
@@ -192,7 +211,7 @@ def _process_single_reference(
                 query_k_nearest_array_indices = nearest_neighbors_array_indices_k_nearest[i]
                 query_k_nearest_reference_indices = reference_indices[query_k_nearest_array_indices]
                 
-                if model == "clip":
+                if model == "clip" or (model == "sae" and faiss_index is None):
                     query_k_similarities = similarities[i, query_k_nearest_array_indices]
                 else:
                     query_k_similarities = similarities[i]
@@ -214,7 +233,9 @@ def find_nearest_neighbors(
         n_queries: int,
         k_nearest: int,
         batch_size: int,
-        save_path: str | None = None):
+        save_path: str | None = None,
+        top_k: int | None = None
+        ):
     '''
     Finds nearest neighbor referenece embedding for each query embedding.
 
@@ -262,7 +283,8 @@ def find_nearest_neighbors(
                 query_indices_fileref=query_indices_fileref,
                 n_queries=n_queries,
                 k_nearest=k_nearest,
-                batch_size=batch_size
+                batch_size=batch_size,
+                top_k=top_k
             )
     
     if save_path is not None:
@@ -358,6 +380,92 @@ def average_precision(
     
     ap /= len(relevant_items)
     return ap
+
+def _process_gt_dict(
+        ground_truth: Dict[int, int],
+        n_reference_paths: int,
+        n_queries: int
+        ):
+    ref_grouping = [{
+        "query_indices": [],
+        "reference_indices": []
+    } for _ in range(n_reference_paths)]
+    
+    for query_idx, ref_idx in ground_truth.items():
+        if query_idx >= REFERENCE_SET_SIZE + n_queries:
+            break
+        ref_set = ref_idx // REFERENCE_SET_SIZE
+        ref_grouping[ref_set]["query_indices"].append(query_idx)
+        ref_grouping[ref_set]["reference_indices"].append(ref_idx)
+    
+    return ref_grouping
+
+def _retrieve_array_indices(
+        type: Literal["query", "reference"],
+        group_matches: Dict[str, List],
+        indices: np.array):
+    group_indices = group_matches[f"{type}_indices"]
+    array_indices = []
+    for idx in group_indices:
+        array_indices.append(np.where(indices == idx)[0][0])
+    
+    return array_indices
+
+def compute_gt_similarities(
+        query_embeddings_path: str,
+        reference_embeddings_paths: List[str],
+        model: Literal['clip', 'sae'],
+        ground_truth: Dict[int, int],
+        n_queries: int,
+        save_path: str | None = None,
+        top_k: int | None = None 
+):
+    if len(reference_embeddings_paths) != 20:
+        raise ValueError(f"Expected 20 refernece paths. Got {len(reference_embeddings_paths)}.")
+    
+    ref_grouping = _process_gt_dict(ground_truth, len(reference_embeddings_paths), n_queries)
+    model_embeddings_filename = model + '.h5'
+    indices_filename = 'indicies.h5' # there was a typo while saving files - using the same name here
+    metric = "manhattan" if model == "sae" else "cosine"
+
+    result = {query_idx: {"gt_ref_idx": ref_idx, "similarity": None} for query_idx, ref_idx in ground_truth.items() if query_idx < REFERENCE_SET_SIZE + n_queries}
+
+    with h5py.File(f"{query_embeddings_path}/{model_embeddings_filename}") as query_embeddings_file, \
+         h5py.File(f"{query_embeddings_path}/{indices_filename}") as query_indices_file:
+        query_embeddings = query_embeddings_file[f"{model}_embeddings"][:]
+        query_indices = query_indices_file["image_indices"][:]
+        
+        for group_matches, reference_embeddings_path in zip(ref_grouping, reference_embeddings_paths):
+            print(f"Processing queries with references from {reference_embeddings_path}...")
+            with h5py.File(f"{reference_embeddings_path}/{model_embeddings_filename}") as reference_embeddings_file, \
+                 h5py.File(f"{reference_embeddings_path}/{indices_filename}") as reference_indices_file:
+                reference_embeddings = reference_embeddings_file[f"{model}_embeddings"][:]
+                reference_indices = reference_indices_file["image_indices"][:]
+
+                group_query_array_indices = _retrieve_array_indices("query", group_matches, query_indices)
+                group_reference_array_indices = _retrieve_array_indices("reference", group_matches, reference_indices)
+                group_query_embeddings = query_embeddings[group_query_array_indices, :]
+                group_reference_embeddings = reference_embeddings[group_reference_array_indices, :]
+                
+                if model == "sae" and top_k is not None:
+                    _filter_top_k_activations(group_query_embeddings, top_k)
+                    _filter_top_k_activations(group_reference_embeddings, top_k)
+                
+                similarites = compute_similarity(group_query_embeddings, group_reference_embeddings, metric)
+
+                for i, query_idx in enumerate(group_matches["query_indices"]):
+                    result[query_idx]["similarity"] = similarites[i, i]
+    
+    if save_path is not None:
+        if top_k is not None:
+            filename = f"gt_similarities_{n_queries}_queries_{model}_{top_k}.pkl"
+        else:
+            filename = f"gt_similarities_{n_queries}_queries_{model}.pkl"
+        print(f"Saving gt similarities to {filename}...")
+        with open(f"{save_path}/{filename}", 'wb') as f:
+            pickle.dump(result, f)
+
+    return result
 
 """def compute_metrics(
         query_embeddings: np.array,
